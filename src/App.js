52895f4c0c6052f50188402fcb1498cc0c562226
Mark Sweet <marksweet@Marks-MacBook-Air.local>
2021-07-14 15:47:19 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './Welcome';
+
+function App() {
+ 
+  const array = ['1', '2', '3']
+  array.forEach(function(element){
+  })
+  return (
+    <div className="App">
+      <Welcome />   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,5 @@
+function Welcome(props){
+    return <h1>hello, {props.name}</h1>
+}
+
+export default Welcome;
